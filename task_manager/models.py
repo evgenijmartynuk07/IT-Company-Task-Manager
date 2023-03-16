a73cf0aac2467b79078a4cb6b92d5430048cec78
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-16 19:43:54 +0200

    add Worker(user), Task, Position model to models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        name="worker"
+    )
+
