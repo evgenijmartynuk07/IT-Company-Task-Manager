d6b1e24227d128b4d0b6252c5ceef81729627586
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-16 21:08:09 +0200

    Add TaskType to admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import TaskType, Task, Worker, Position
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
