83574207aed14ff16f196b3422330010e45b468c
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-16 20:22:14 +0200

    add name to str method for TaskType

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class TaskType(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=63, unique=True)
