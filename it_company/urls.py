d61f26b4294670a4b84a3f1b82ede7a10766029b
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-18 21:01:09 +0200

    Add registration and html login

--- it_company/urls.py
+++ it_company/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
+    path("accounts/", include("django.contrib.auth.urls")),
 
     path('__debug__/', include('debug_toolbar.urls')),
 ]
