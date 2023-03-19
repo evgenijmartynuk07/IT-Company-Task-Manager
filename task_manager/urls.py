81cdd2ec199f92f40d1983b6c7566cf5b9f18403
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-19 18:26:52 +0200

    Change templates task_detail and task_list

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("worker/", WorkerListView.as_view(), name="worker-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
-    path("task/task/", TaskListView.as_view(), name="task-list"),
+    path("task/", TaskListView.as_view(), name="task-list"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
