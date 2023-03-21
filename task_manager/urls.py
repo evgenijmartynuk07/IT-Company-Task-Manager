89ce1087f087078f9933cfa1d6db0f8bfe5ee635
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-03-21 22:08:21 +0200

    Change worker page create worker update url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from task_manager.views import index, \
     TaskCreateView, \
-    TaskUpdateView, TaskDetailView, TaskDeleteView, WorkerCreateView, WorkerDetailView, WorkerListView, TaskListView
+    TaskUpdateView, TaskDetailView, TaskDeleteView, WorkerCreateView, WorkerDetailView, WorkerListView, TaskListView, WorkerUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("worker/create/", WorkerCreateView.as_view(), name="worker-create"),
     path("worker/", WorkerListView.as_view(), name="worker-list"),
     path("worker/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("worker/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("task/", TaskListView.as_view(), name="task-list"),
